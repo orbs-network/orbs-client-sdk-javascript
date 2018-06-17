6c04ff9a80986d588fba1db1a6a120bb7beb8480
Kirill Maksimov <kirill@orbs.com>
2018-06-17 15:22:49 +0300

    Update download script

--- download-crypto-sdk.sh
+++ download-crypto-sdk.sh
@@ -4,6 +4,8 @@ export CRYPTO_SDK_VERSION=${CRYPTO_SDK_VERSION-22ded44b307d629e613dacfe8cca8cc56
 
 export S3_PATH=s3://orbs-client-sdk/lib/$CRYPTO_SDK_VERSION
 
+pip install awscli
+
 rm -rf native
 aws s3 sync $S3_PATH native
 
