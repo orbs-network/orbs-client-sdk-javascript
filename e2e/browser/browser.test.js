07561fe1131fcf8f25e109bdc312eba4214d2d76
Gil Amran <gil@orbs.com>
2019-01-20 16:26:27 +0200

    Added demo puppeteer tests

--- e2e/browser/browser.test.js
+++ e2e/browser/browser.test.js
@@ -1,13 +1,24 @@
 const path = require("path");
 
-describe("E2E browser", () => {
+const runStep = async step => await page.click(`#step${step} .action-button`);
+const getStepResult = async step => await page.$eval(`#step${step} .result`, el => el.innerText);
 
+describe("E2E browser", () => {
   beforeAll(async () => {
     await page.goto(`file:${path.join(__dirname, "index.html")}`);
   });
 
-  it("should display 'success' on page", async () => {
-    await expect(page).toMatch("success");
+  it("should create an account", async () => {
+    await runStep(1);
+    const accountBytes = await getStepResult(1);
+    const accountIdLength = accountBytes.split(",").length;
+    expect(accountIdLength).toEqual(32);
   });
 
+  it("should create an account 2", async () => {
+    await runStep(2);
+    const accountBytes = await getStepResult(2);
+    const accountIdLength = accountBytes.split(",").length;
+    expect(accountIdLength).toEqual(32);
+  });
 });
