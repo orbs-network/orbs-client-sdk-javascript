29b7c00ef91855c22071be48434da6df3f2641b0
itamar Arjuan <itamar.arjuan@gmail.com>
2019-01-21 16:22:03 +0200

    adding installation of newer node

---
+++ .circleci/install-current-node.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+sudo apt-get install curl python-software-properties
+curl -sL https://deb.nodesource.com/setup_11.x | sudo -E bash -
+sudo apt-get update && sudo apt-get install -y nodejs
