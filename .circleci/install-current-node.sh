1cf53de959fc531ee5753ecc8bb32cf47206623e
itamar Arjuan <itamar.arjuan@gmail.com>
2019-01-21 16:34:17 +0200

    testing out nvm

--- .circleci/install-current-node.sh
+++ .circleci/install-current-node.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 
-sudo apt-get install curl python-software-properties
-curl -sL https://deb.nodesource.com/setup_11.x | sudo -E bash -
-sudo apt-get update && sudo apt-get install -y nodejs
+sudo nvm
+exit 1
