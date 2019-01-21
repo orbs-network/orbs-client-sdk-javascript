65089b23e4914ed785733789006598eae9e8010e
itamar Arjuan <itamar.arjuan@gmail.com>
2019-01-21 16:54:22 +0200

    adding main sh

---
+++ .circleci/main.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+export NVM_DIR="/opt/circleci/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+nvm use 11.7
+npm install
+bash test.sh
