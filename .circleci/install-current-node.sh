ff47e33508bf1c2462c0334cbf947e1af9cfb962
itamar Arjuan <itamar.arjuan@gmail.com>
2019-01-21 16:39:51 +0200

    trying to install latest node

--- .circleci/install-current-node.sh
+++ .circleci/install-current-node.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
 
 wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.34.0/install.sh | bash
-nvm
+
+export NVM_DIR="/opt/circleci/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+nvm install 11.7
