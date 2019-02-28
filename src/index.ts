8b2424fa9089f0f4c4570825dcd66e8f0ef2e202
Gil Amran <gil@orbs.com>
2019-02-28 18:09:36 +0200

    exported ExecutionResult

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export { createAccount, addressToBytes, bytesToAddress } from "./orbs/Account";
+export { ExecutionResult } from "./codec/ExecutionResult";
 export { Client } from "./orbs/Client";
 export { argUint32, argUint64, argString, argBytes, argAddress } from "./codec/Arguments";
 export { NetworkType } from "./codec/NetworkType";
