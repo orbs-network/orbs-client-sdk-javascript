fed6945802aca71525be2c154e42ba75c188c85a
Gil Amran <gil@orbs.com>
2019-03-11 17:22:07 +0200

    version 0.0.5, added RequestStatus to the exported types

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 export { createAccount, addressToBytes, bytesToAddress } from "./orbs/Account";
 export { ExecutionResult } from "./codec/ExecutionResult";
+export { RequestStatus } from "./codec/RequestStatus";
 export { Client } from "./orbs/Client";
 export { argUint32, argUint64, argString, argBytes, argAddress } from "./codec/Arguments";
 export { NetworkType } from "./codec/NetworkType";
